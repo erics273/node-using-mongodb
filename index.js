73836615e0eeb04ff300601f358963aecaf83206
Eric Schwartz <eric@developintelligence.com>
2021-09-21 13:32:12 -0400

    m2:

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Successfully Connected to MongoDB`)
+  }).catch(() => {
+    console.log(`Error Connecting to the MongoDB`)
+  })
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
